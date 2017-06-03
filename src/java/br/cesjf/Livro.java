404d6abd48c0cdeedcbd1dca638b6564dc86b300
BrunahAlves <brunahalves94@gmail.com>
2017-06-02 22:02:05 -0300

    Configurações iniciais JPA

---
+++ src/java/br/cesjf/Livro.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cesjf;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author alunoces
+ */
+@Entity
+public class Livro implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String autor;
+    private Integer ano;
+
+    public Livro() {
+       this.id=null; 
+       this.titulo=""; 
+       this.autor=""; 
+       this.ano=null; 
+       
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+    
+    
+    
+    
+}
